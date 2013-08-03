cde3838a671056edc858afd073f7fb90c79fa04e
Tochukwu Okoro <tcokoro@gmail.com>
2013-08-03 11:46:43 -0700

    synchronized coding in the browser

--- crackit/config/routes.rb
+++ crackit/config/routes.rb
@@ -1,4 +1,5 @@
 Crackit::Application.routes.draw do
+  get '/testSync' => "code_rooms#testSynchronization"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +49,7 @@ Crackit::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'code_rooms#new'
 
   # See how all your routes lay out with "rake routes"
 
