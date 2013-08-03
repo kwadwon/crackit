c7108b37aa52dc0108b45779246f4627248c6e50
kwadwo <kwadwon@gmail.com>
2013-08-03 04:53:01 -0700

    kwadwo - removed some files

--- crackit/config/routes.rb
+++ crackit/config/routes.rb
@@ -1,7 +1,7 @@
 Crackit::Application.routes.draw do
 root :to => 'login#start'
 resources :login
-resources :video
+    #resources :video
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -58,5 +58,12 @@ resources :video
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+     resources :video do
+       collection do
+         get 'show'
+		 get 'join'
+       end
+     end
+
+   # match ':controller(/:action(/:id))(.:format)'
 end
