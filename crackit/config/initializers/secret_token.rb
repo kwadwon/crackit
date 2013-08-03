d40b5bac447ec39efcf0e7f30512788784f14768
kwadwo <kwadwon@gmail.com>
2013-08-02 21:34:19 -0700

    created rails project

---
+++ crackit/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crackit::Application.config.secret_token = 'e8d3d7287c5f997dc7224cacde2bc4f2175627bb15d6738f01be4897573fcf6515ee6e61d68ed85cff4bfe19b70df602dfffab854895880e6682021c2aa41c08'
